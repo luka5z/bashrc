349c0f66b7e2a56e25373501fbf723169b968dfb
luka5z <lzachulski@gmail.com>
2017-02-15 12:43:42 +0100

    Use Git configuration for defining aliases

--- .bash_aliases
+++
@@ -1,3 +0,0 @@
-# Git aliases
-alias git-log="git log --graph --decorate --name-status"
-alias git-short-log="git log --graph --oneline --decorate"
---
+++ git-aliases/git-aliases.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+git config --global alias.graphlog   'log --graph --oneline --decorate'
+git config --global alias.sgraphlog  'log --graph --decorate --name-status'
